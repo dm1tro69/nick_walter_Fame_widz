b05abaa7ede6308e3850303355c30c6e1f151403
dm1tro69 <dimolg22@gmail.com>
2020-02-05 22:31:52 +0200

    class views

---
+++ halls/views.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
+from .models import Hall
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+
+
+def home(request):
+    return render(request, 'halls/home.html', {})
+
+
+def dashboard(request):
+    return render(request, 'halls/dashboard.html', {})
+
+
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
+
+
+#def create_hall(request):
+
+class CreateHall(CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
+
+
+
+class DetailHall(DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'
+
